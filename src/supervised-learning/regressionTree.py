208f32f1b467220d4b2444787b35d35f95b3006d
BrianHadianSTEI23 <bri.hadian@gmail.com>
2025-09-05 14:15:51 +0700

    feat: finally regression tree finished

--- src/supervised-learning/regressionTree.py
+++ src/supervised-learning/regressionTree.py
@@ -1,131 +1,118 @@
 
+
 import numpy as np
+import pandas as pd
+
+class Point : 
+    
+    def __init__(self, x : dict) -> None:
+        self.value = x
 
 class Node : 
-    def __init__(self, featureIndex = None, threshold= None, left=None, right=None, infoGain = None, value = None):
-        # for decision node
-        self.featureIndex = featureIndex,
-        self.threshold = threshold
+    
+    def __init__(self, points : list[Point], feature = None, left = None, right = None) -> None: # left and right is of type Node (recursive datatype)
+        self.points = points
         self.left = left
         self.right = right
-        self.infoGain = infoGain
+        self.feature = feature
 
-        # for leaf
-        self.value = value
+    def calculateCurrentFeatVariance(self): 
+        # count the variance
+        values = np.array([p.value[self.feature] for p in self.points])
+        return  np.var(values)
 
+    def calculateCurrentFeatMean(self) : 
+        values = np.array([p.value[self.feature] for p in self.points])
+        return  np.mean(values)
 
-class ClassificationTree : 
-    def __init__(self, minSampleSplit = 2, maxDepth = 10**5) -> None:
-        self.root = None,
-        self.minSampleSplit = minSampleSplit
+class RegressionTree : 
+
+    # this regression tree assume left is true and right is false
+
+    def __init__(self, features : list[str], maxDepth : int, minSample : int, maxVariance : float = 0.05) -> None:
+        
+        self.features = features
         self.maxDepth = maxDepth
+        self.minSample = minSample
+        self.MAX_VARIANCE = maxVariance
+        self.FEAT_MAX_VARIANCE = maxVariance
 
-    def buildTree(self, dataset, currDepth = 0) :
-        
-        X, Y = dataset[:,:-1], dataset[:, -1]
-        numSamples, numFeatures = np.shape(X)
+    def train(self, dataset : pd.DataFrame) :
 
-        if numSamples >= self.minSampleSplit and currDepth < self.maxDepth :
-            bestSplit = self.getBestSplit(dataset, numSamples, numFeatures)
-            if bestSplit['infoGain'] > 0 :
-                # recur left
-                leftSubtree = self.buildTree(bestSplit["dataset_left"], currDepth+1)
-                # recur right
-                rightSubtree = self.buildTree(bestSplit["dataset_right"], currDepth+1)
-                return Node(bestSplit["featureIndex"], bestSplit["threshold"], leftSubtree, rightSubtree, bestSplit["infoGain"])
+        # init : push all Point (rows) into root node
+        points = []
+        for _, row in dataset[self.features].iterrows():
+            points.append(Point(row.to_dict()))
+        root = Node(points)
 
-        leaf_val = self.calculateLeafValue(Y)
-        return Node (value=leaf_val)
+        # based on the features, pick one feature and calculate the variance of that particular feture according to the current sample
+        self.split(root)
 
+        return root
     
-    def getBestSplit(self, dataset, numSamples, numFeatures):
-        
-        # dict to store the best split
-        bestSplit = {}
-        maxInfoGain = -float("inf")
-        
-        # loop over all features
-        for featureIndex in range(numFeatures):
-            featureValues = dataset[:, featureIndex]
-            possibleThresholds = np.unique(featureValues)
+    def split(self, node : Node) : 
 
-            for threshold in possibleThresholds:
-                # get current split
-                datasetLeft, datasetRight = self.split(dataset, featureIndex, threshold)
-                # check if childs are not null
-                if len(datasetLeft)>0 and len(datasetRight)>0:
-                    y, left_y, right_y = dataset[:, -1], datasetLeft[:, -1], datasetRight[:, -1]
-                    
-                    # get information gain
-                    currInfoGain = self.informationGain(y, left_y, right_y, "gini")
-                    
-                    # update the best split if needed
-                    if currInfoGain>maxInfoGain:
-                        bestSplit["feature_index"] = featureIndex
-                        bestSplit["threshold"] = threshold
-                        bestSplit["datasetLeft"] = datasetLeft
-                        bestSplit["datasetRight"] = datasetRight
-                        bestSplit["infoGain"] = currInfoGain
-                        maxInfoGain = currInfoGain
-                        
-        return bestSplit
+        if len(node.points) < self.minSample:
+            return
+
+        bestGain = 0
+        bestFeature = None
+        bestLeft : list[Point ]= []
+        bestRight : list[Point]= []
+
+        parentVar = np.var([np.mean(list(p.value.values())) for p in node.points])  
+
+        for feature in self.features:
+
+            #  if the variance is high enough, get the mean of that particular feature (split point = mean of that feature)
+            splitPoint = np.mean(np.array([p.value[feature] for p in node.points]))
+
+            # branching : left tree contains list of Point that is true according to the branch, and the right is false
+
+            # points for left (true)
+            leftPoints = []
+            rightPoints = []
+            for p in node.points:
+                if p.value[feature] < splitPoint :
+                    leftPoints.append(p)
+                else :
+                    rightPoints.append(p)
+            
+            if len(leftPoints) == 0 or len(rightPoints) == 0:
+                continue
+
+            # do split again for left (recur)
+            leftVar = np.var([p.value[feature] for p in leftPoints])
+            rightVar = np.var([p.value[feature] for p in rightPoints])
+            weightingVar = ((len(leftPoints) / len(node.points)) * leftVar) + ((len(rightPoints) / len(node.points)) * rightVar)
+
+            currGain = weightingVar - parentVar
+
+            # calculate the variance each tree and sum it. if it's >  currFeatVariance, split again, else prune
+            if currGain > bestGain: 
+                # do split again for right and left(recur)
+                currGain = bestGain
+                bestFeature = feature
+                bestLeft = leftPoints
+                bestRight = rightPoints
+
+        # if no gain, stop
+        if bestFeature is None or bestGain < self.MAX_VARIANCE:
+            node.feature = None
+            return    
+        
+        # now all featu has been tested, push the node and split if it could
+        node.feature = bestFeature
+        node.left = Node(bestLeft)
+        node.right = Node(bestRight)
+
+        # recursive split
+        self.split(node.left)
+        self.split(node.right)
     
-    # split dataset based on feature index and threshold
-    def split(self, dataset, featureIndex, threshold):
+    def predict(self, x : np.ndarray) :
+
+
+
+        return
         
-        datasetLeft = np.array([row for row in dataset if row[featureIndex]<=threshold])
-        datasetRight = np.array([row for row in dataset if row[featureIndex]>threshold])
-        return datasetLeft, datasetRight
-    
-    # compute information gain
-    def informationGain(self, parent, lChild, rChild, mode="entropy"):
-        
-        weightLeft = len(lChild) / len(parent)
-        weightRight = len(rChild) / len(parent)
-        if mode=="gini":
-            gain = self.giniIndex(parent) - (weightLeft*self.giniIndex(lChild) + weightRight*self.giniIndex(rChild))
-        else:
-            gain = self.entropy(parent) - (weightLeft*self.entropy(lChild) + weightRight*self.entropy(rChild))
-        return gain
-    
-    def entropy(self, y):
-        
-        classLabels = np.unique(y)
-        entropy = 0
-        for cls in classLabels:
-            pCls = len(y[y == cls]) / len(y)
-            entropy += -pCls * np.log2(pCls)
-        return entropy
-    
-    def giniIndex(self, y):
-        
-        classLabels = np.unique(y)
-        gini = 0
-        for cls in classLabels:
-            pCls = len(y[y == cls]) / len(y)
-            gini += pCls**2
-        return 1 - gini
-        
-    def calculateLeafValue(self, Y):
-        
-        Y = list(Y)
-        return max(Y, key=Y.count)
-    
-    def fit(self, X, Y):
-        
-        dataset = np.concatenate((X, Y), axis=1)
-        self.root = self.buildTree(dataset)
-    
-    def predict(self, X):
-        
-        preditions = [self.makePrediction(x, self.root) for x in X]
-        return preditions
-    
-    def makePrediction(self, x, tree):
-        
-        if tree.value!=None: return tree.value
-        feature_val = x[tree.feature_index]
-        if feature_val<=tree.threshold:
-            return self.makePrediction(x, tree.left)
-        else:
-            return self.makePrediction(x, tree.right)
